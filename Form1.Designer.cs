b0a5861804efc73a8e997ffb499f870f3a9784c5
Joao Gaspar <61797706+jgaspar10@users.noreply.github.com>
2023-05-30 17:26:18 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace BoldStrings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.transformButton = new Guna.UI2.WinForms.Guna2Button();
+            this.clearButton = new Guna.UI2.WinForms.Guna2Button();
+            this.rtbProcessedTxt = new System.Windows.Forms.RichTextBox();
+            this.txtInput = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2GradientPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            this.guna2GradientPanel1.Controls.Add(this.panel1);
+            this.guna2GradientPanel1.Controls.Add(this.transformButton);
+            this.guna2GradientPanel1.Controls.Add(this.clearButton);
+            this.guna2GradientPanel1.Controls.Add(this.rtbProcessedTxt);
+            this.guna2GradientPanel1.Controls.Add(this.txtInput);
+            this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2GradientPanel1.FillColor = System.Drawing.Color.White;
+            this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.White;
+            this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(800, 450);
+            this.guna2GradientPanel1.TabIndex = 0;
+            // 
+            // transformButton
+            // 
+            this.transformButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.transformButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.transformButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.transformButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.transformButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.transformButton.ForeColor = System.Drawing.Color.White;
+            this.transformButton.Location = new System.Drawing.Point(409, 367);
+            this.transformButton.Name = "transformButton";
+            this.transformButton.Size = new System.Drawing.Size(164, 40);
+            this.transformButton.TabIndex = 3;
+            this.transformButton.Text = "Transform";
+            this.transformButton.Click += new System.EventHandler(this.transformButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.clearButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.clearButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.clearButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.clearButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.ForeColor = System.Drawing.Color.White;
+            this.clearButton.Location = new System.Drawing.Point(234, 367);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(164, 40);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // rtbProcessedTxt
+            // 
+            this.rtbProcessedTxt.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbProcessedTxt.Location = new System.Drawing.Point(205, 246);
+            this.rtbProcessedTxt.Name = "rtbProcessedTxt";
+            this.rtbProcessedTxt.Size = new System.Drawing.Size(387, 100);
+            this.rtbProcessedTxt.TabIndex = 1;
+            this.rtbProcessedTxt.Text = "";
+            // 
+            // txtInput
+            // 
+            this.txtInput.AcceptsReturn = true;
+            this.txtInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtInput.DefaultText = "";
+            this.txtInput.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtInput.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtInput.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtInput.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtInput.FillColor = System.Drawing.Color.LightGray;
+            this.txtInput.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtInput.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.txtInput.ForeColor = System.Drawing.Color.Black;
+            this.txtInput.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtInput.Location = new System.Drawing.Point(205, 121);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.PasswordChar = '\0';
+            this.txtInput.PlaceholderForeColor = System.Drawing.Color.Gray;
+            this.txtInput.PlaceholderText = "Insert text here...";
+            this.txtInput.SelectedText = "";
+            this.txtInput.Size = new System.Drawing.Size(387, 118);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(100, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(607, 33);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bold string converter for enhanced reading";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 82);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guna2GradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.guna2GradientPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Guna.UI2.WinForms.Guna2Button transformButton;
+        private Guna.UI2.WinForms.Guna2Button clearButton;
+        private System.Windows.Forms.RichTextBox rtbProcessedTxt;
+        private Guna.UI2.WinForms.Guna2TextBox txtInput;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoldStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void transformButton_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text; // This line gets input from user
+            string[] words = input.Split(' '); // This line splits the input string into individual words and stores them in a string array called "words"
+            // The split() method also takes a delimeter character which in this case is a space (' ')
+            var output = new StringBuilder();// This Line creates a new StringBuilder object named output, we will use it below to construct the output.
+            foreach (string word in words) // This Line starts a loop that will iterate over each word in the words array.
+            {
+                string boldword = "";
+                if (word.Length >= 3) // Makes the first 3 letters bold
+                {
+                    boldword = string.Format(@"\b {0}\b0 {1}", word.Substring(0, 3), word.Substring(3));
+                }
+                else // Makes the first letter bold only
+                {
+                    boldword = string.Format(@"\b {0}\b0 {1}", word[0], word.Substring(1)); // this if statement processes each word in the loop. 
+                } // if the word has 3 or more letters the first 3 letters are emphasided in bold by the RFT tags \b and \b0 and if it has fewer than 3 enphasise first letter only.
+                output.Append(boldword).Append(" ");// this line will append the emphasided word followed by a space to the stringbuilder named output.
+            } // the append method will add the string to the String Builder.
+
+            rtbProcessedTxt.Rtf = @"{\rtf1\ansi " + output.ToString() + "}"; // This line of code sets the RTF property of the RichTextBox named "rtbProcessedTxt.Rtf"
+        } // to the processed string wrapped in the RTF header and footer and i used tostring method to get the final output string from the stringbuilder.
+        // because i am using a richtextbox, the rtf format must be used to allow us to specify the font style for specific parts of the text.
+        // in this case \b tag to start the bold text and \b0 to end it.
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+        }
+    }
+}
